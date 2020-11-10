9487fb775b29b91ae58641412c6c1a634fdaac5e
KukharchukDaniil <56891342+KukharchukDaniil@users.noreply.github.com>
2020-11-10 14:11:37 +0300

    Login

--- src/Login.java
+++ src/Login.java
@@ -6,7 +6,9 @@ public class Login extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
     private JButton buttonCancel;
-    private JTextField textField1;
+    private JFormattedTextField formattedTextField1;
+    private JFormattedTextField formattedTextField2;
+    private JPanel addressField;
     boolean bOkFlag = false;
 
     public Login() {
@@ -16,7 +18,7 @@ public class Login extends JDialog {
 
         buttonOK.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                onOK();
+                //onOK();
             }
         });
         buttonCancel.addActionListener(new ActionListener() {
@@ -43,15 +45,15 @@ public class Login extends JDialog {
         setTitle("Enter the address");
     }
 
-    private void onOK() {
-        String str;
-        if(!(str = textField1.getText()).isEmpty())
-        {
-            bOkFlag = true;
-            Client.address = str;
-        }
-        dispose();
-    }
+//    private void onOK() {
+//        String str;
+//        if(!(str = textField1.getText()).isEmpty())
+//        {
+//            bOkFlag = true;
+//            Client.address = str;
+//        }
+//        dispose();
+//    }
 
     private void onCancel() {
         // add your code here if necessary
